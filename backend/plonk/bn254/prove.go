891184b778909c70226e6666b88cda6bc649ff4c
tumberger <jens.ernstberger@tum.de>
2023-12-17 14:53:30 +0100

    temp no dave to txt

--- backend/plonk/bn254/prove.go
+++ backend/plonk/bn254/prove.go
@@ -26,7 +26,7 @@ import (
 	"runtime"
 	"sync"
 	"time"
-	"os"
+	// "os"
 
 	"golang.org/x/sync/errgroup"
 
@@ -511,25 +511,25 @@ func saveCoefficients(id string, coeffs []fr.Element) {
 		return
 	}
 
-	filename := fmt.Sprintf("coefficients_Two_%s.txt", id)
-	file, err := os.Create(filename)
-	if err != nil {
-		fmt.Printf("Error creating file %s: %v\n", filename, err)
-		return
-	}
-	defer file.Close()
+	// filename := fmt.Sprintf("coefficients_Two_%s.txt", id)
+	// file, err := os.Create(filename)
+	// if err != nil {
+	// 	fmt.Printf("Error creating file %s: %v\n", filename, err)
+	// 	return
+	// }
+	// defer file.Close()
 
-	// coeffs := p.Coefficients()
+	// // coeffs := p.Coefficients()
 
-	for _, coeff := range coeffs {
-		_, err := file.WriteString(fmt.Sprintf("%v\n", coeff))
-		if err != nil {
-			fmt.Printf("Error writing to file %s: %v\n", filename, err)
-			return
-		}
-	}
+	// for _, coeff := range coeffs {
+	// 	_, err := file.WriteString(fmt.Sprintf("%v\n", coeff))
+	// 	if err != nil {
+	// 		fmt.Printf("Error writing to file %s: %v\n", filename, err)
+	// 		return
+	// 	}
+	// }
 
-	fmt.Printf("Coefficients saved to %s\n", filename)
+	fmt.Printf("Coefficients saved to %s\n", id)
 }
 
 
@@ -1135,6 +1135,7 @@ func (s *instance) computeNumerator() (*iop.Polynomial, error) {
 
 	fmt.Printf("LOGGER: took=%v MSG=TIME TO GATE, ORDER, RATIO CONSTRAINT\n", time.Duration(time.Since(thisStart).Nanoseconds()))
 
+	thisStartTwo := time.Now()
 	// scale everything back
 	go func() {
 		for i := id_ZS; i < len(s.x); i++ {
@@ -1160,8 +1161,13 @@ func (s *instance) computeNumerator() (*iop.Polynomial, error) {
 		close(s.chRestoreLRO)
 	}()
 
+	fmt.Printf("LOGGER: took=%v MSG=TIME TO SCALE ALL BACK\n", time.Duration(time.Since(thisStartTwo).Nanoseconds()))
+
 	// ensure all the goroutines are done
 	wgBuf.Wait()
+
+	fmt.Printf("LOGGER: took=%v MSG=TIME TO WAIT FOR GOROUTINES\n", time.Duration(time.Since(thisStartTwo).Nanoseconds()))
+
 
 	res := iop.NewPolynomial(&cres, iop.Form{Basis: iop.LagrangeCoset, Layout: iop.BitReverse})
 
